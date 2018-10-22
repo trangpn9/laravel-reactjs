ad6b7fed95f2cdd61a0680a96d237df6f12d113c
Phan Nhu Trang <trangpn90@gmail.com>
2018-10-23 04:31:44 +0700

    Commit nod_modules and verdon

---
+++ storage/framework/views/d6c132150c7ccb74d8c10137b689f50195879f26.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="<?php echo e(app()->getLocale()); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel - React_JS</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css" />
+        <link href="./css/app.css" rel="stylesheet" type="text/css"/>
+        
+    </head>
+    <body>                       
+        <div class="container">
+            <div class="row">
+                <div id="app" class="col-lg-12"></div>
+            </div>            
+        </div>
+
+        <script type="text/javascript" src="./js/app.js"></script>
+    </body>
+</html>
